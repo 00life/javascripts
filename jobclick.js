95718ec8ad39ea7625fae2c3830a39be2af35ff3
00life <116957019+00life@users.noreply.github.com>
2024-02-07 16:37:22 -0500

    Update jobclick.js

--- jobclick.js
+++ jobclick.js
@@ -18,7 +18,7 @@ setInterval(()=>{
       setTimeout(()=>{},200);
 
       let subject = anchorText[i].innerText.toLowerCase().trim();
-      let school = anchorText[i].parentElement.parentElement.children[2];
+      let school = anchorText[i].parentElement.parentElement.children[2].children[0].innerHTML;
       console.log(school);
   
       if(subject == "library"){
