a04541c6bd6c43d4bec4d6ffc22f961730462e1f
00life <116957019+00life@users.noreply.github.com>
2024-09-12 14:24:43 -0400

    Update hwcdsbbutons.js

--- hwcdsbbutons.js
+++ hwcdsbbutons.js
@@ -3,7 +3,7 @@ let btn = document.createElement('input');
 btn.setAttribute('type','button');
 btn.setAttribute('value','time');
 btn.addEventListener('click',e=>{
-    let date = new Date().toLocaleTimeString();
+    let date = new Date().toLocaleTimeString().slice(0,-6);
     console.log(e.currentTarget.value = date);
 });
 
