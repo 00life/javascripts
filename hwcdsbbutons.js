c6f4bc934afd93f3c05f5d66e82bd859f317a8d8
00life <116957019+00life@users.noreply.github.com>
2024-09-12 17:03:41 -0400

    Update hwcdsbbutons.js

--- hwcdsbbutons.js
+++ hwcdsbbutons.js
@@ -4,16 +4,12 @@ Array.from(document.querySelectorAll('td.seat-cellName')).forEach(e1=>{
     btn.setAttribute('type','button');
     btn.setAttribute('value','TIME');
     btn.setAttribute('class','myBtn');
-    btn.setAttribute('data-min', 0);
     btn.addEventListener('click',e2=>{
         let date = new Date().toLocaleTimeString().slice(0,-6);
         if(e2.currentTarget.value =='TIME'){
             e2.currentTarget.value = date;
-            btn.setAttribute('data-min', Math.round(new Date().getTime() / 1000 / 60));
         }else{
             e2.currentTarget.value = 'TIME'};
-            btn.setAttribute('data-min',0);
-      
     });
 
     let ele_div = document.createElement('div');
@@ -23,10 +19,12 @@ Array.from(document.querySelectorAll('td.seat-cellName')).forEach(e1=>{
 
 setInterval(()=>{
     Array.from(document.querySelectorAll('.myBtn')).forEach(e3=>{
-        let current =  Math.round(new Date().getTime() / 1000 / 60);
-        (current > e3.currentTarget.dataset.min + 1)? e3.currentTarget.style['background-color'] = 'red' : null;
+        let current =  let date = new Date().toLocaleTimeString().slice(0,-6);
+        console.log('test ', e3.dataset);    
+        console.log(e3.style.color='red');
+        
     });
-}, 10000);
+}, 5000);
 
 //PASTE CODE BELOW
 //fetch('https://raw.githubusercontent.com/00life/javascripts/master/hwcdsbbutons.js').then(r=>r.text()).then(r=>eval(r))
