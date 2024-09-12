81db1fcc9f516d81803b504329189bc4dac3063e
00life <116957019+00life@users.noreply.github.com>
2024-09-12 15:45:58 -0400

    Update hwcdsbbutons.js

--- hwcdsbbutons.js
+++ hwcdsbbutons.js
@@ -1,10 +1,14 @@
-let btn = document.createElement('input');
+Array.from(document.querySelectorAll('td.seat-cellName')).forEach(e1=>{
 
-btn.setAttribute('type','button');
-btn.setAttribute('value','time');
-btn.addEventListener('click',e=>{
-    let date = new Date().toLocaleTimeString().slice(0,-6);
-    console.log(e.currentTarget.value = date);
+    let btn = document.createElement('input');
+    btn.setAttribute('type','button');
+    btn.setAttribute('value','TIME');
+    btn.addEventListener('click',e2=>{
+        let date = new Date().toLocaleTimeString().slice(0,-6);
+        (e2.currentTarget.value =='TIME') ? e2.currentTarget.value = date : e2.currentTarget.value = 'TIME';
+    });
+
+    let ele_div = document.createElement('div');
+    ele_div.appendChild(btn);
+    e1.appendChild(ele_div);
 });
-
-document.querySelector('td.seat-cellName').appendChild(btn)
