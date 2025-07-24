d51f7a71023226758a94612615a49dea39b97e17
00life <116957019+00life@users.noreply.github.com>
2025-07-24 05:17:43 -0400

    Create index.js

---
+++ phaser_games/flappyBird/index.js
@@ -0,0 +1,34 @@
+const GRAVITY_Y = 600;
+const WIDTH = 800;
+const HEIGHT = 500;
+const BIRD_POS = { x: WIDTH /10, y: HEIGHT/2 };
+
+const SHARED_CONFIG = {
+    width: WIDTH,
+    height: HEIGHT,
+    startPosition: BIRD_POS
+};
+
+const config = {
+    type: Phaser.AUTO,
+    ...SHARED_CONFIG,
+    pixelArt: true,
+    parent: 'game-container',
+    physics: {
+        default:"arcade",
+        arcade: {
+            gravity: { y: GRAVITY_Y },
+            debug: true
+        }
+    },
+    scene: [
+        PreloadScene, 
+        new MenuScene(SHARED_CONFIG), 
+        new PlayScene(SHARED_CONFIG),
+        new ScoreScene(SHARED_CONFIG),
+        new PauseScene(SHARED_CONFIG)
+    ]
+};
+
+const game = new Phaser.Game(config);
+
